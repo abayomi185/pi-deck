aefbfe559949175eca674576e21c3fad5dc2ce7a
Yomi Ikuru <abayomi.ikuru@gmail.com>
2020-11-28 00:40:00 +0000

    Add Client Files

---
+++ Software/Client/client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+import socket
+
+HOST = '10.0.1.5'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
